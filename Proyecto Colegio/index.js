12e1272b4c384ce646792f195df114f887d2d0c5
YoxD1234 <79913828+YoxD1234@users.noreply.github.com>
2022-02-19 15:52:14 -0500

    Add files via upload

---
+++ Proyecto Colegio/index.js
@@ -0,0 +1,8 @@
+let items = document.querySelector('#items');
+let menu_bar = document.querySelector('#menu-bar');
+
+    menu_bar.addEventListener('click', function(){
+        items.classList.toggle('menu-toggle');
+
+    })
+   
